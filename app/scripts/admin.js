a2bad660bb7d0d2b760a6e59896bdbba5db5ecbb
fishlegz <velvetwall@protonmail.com>
2017-03-20 21:01:05 +0000

    Scroll slick ads

--- app/scripts/admin.js
+++ app/scripts/admin.js
@@ -1,7 +1,53 @@
 // Admin
 (function() {
+    $('.admin-a').slick({
+      dots: false,
+      arrows: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 8,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+    
     var $uploadForm = $('.add-a-form.upload-a'),
-    $adminSuccess = $('.admin-success');
+        $targetFrom   = $('.target-user-form'),
+        $adminSuccess = $('.admin-success');
+    
+    $('.target-users').on('click', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+                
+        $(this).parent().parent().find('.target-user-form').addClass('visible');
+        $(this).parent().parent().addClass('overlay');
+    });
     
     $('.add-a').on('click', function(e) {
         e.preventDefault();
@@ -11,6 +57,15 @@
     });
     
     $('html').on('click', function(e) {
+        var $targetFormVisible = $('.target-user-form.visible');
+        
+        if( $targetFormVisible ) {
+            e.stopPropagation();
+            e.preventDefault();
+            $targetFormVisible.parent().removeClass('overlay');
+            $targetFormVisible.removeClass('visible');
+        }
+        
         if( $uploadForm.hasClass('visible') ) {
             e.stopPropagation();
             e.preventDefault();
@@ -21,19 +76,21 @@
         return;
     });
     
-    $adminSuccess.on('mouseover', '.success-item.filled', function(e) {
-        var $that = $(this);
-        e.stopPropagation();
-        e.preventDefault();
-        console.log(e.target);
-        
-        if(e.target != this) {
-            return;
-        }
-
-        $('.success-details').fadeOut();
-        $that.find('.success-details').fadeIn();
-    });
+    if($adminSuccess) {
+        $adminSuccess.on('mouseover', '.success-item.filled', function(e) {
+            var $that = $(this);
+            e.stopPropagation();
+            e.preventDefault();
+            console.log(e.target);
+            
+            if(e.target != this) {
+                return;
+            }
+    
+            $('.success-details').fadeOut();
+            $that.find('.success-details').fadeIn();
+        });
+    }
     
     $('.close-details').on('click', function(e) {
         $(this).parent().parent().parent().fadeOut();
